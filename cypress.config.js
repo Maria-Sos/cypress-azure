bb38639203a6634fcab11a017dd407a52a263e22
mariaSOS <ms2145051478@gmail.com>
2023-05-18 22:22:59 -0500

    config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  integrationFolder: "cypress/integration/examples",
+  reporter: "junit",
+  reporterOptions: {
+    mochaFile: "tests/test-output-[hash].xml",
+    toConsole: true,
+    attachments: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
